133287151c057f6fc7287ff11b438ac8f672ab57
BtotheBohne <bdbohnen@gmail.com>
2020-06-07 20:41:26 -0700

    debub.py revision 1

--- debug.py
+++ debug.py
@@ -1,20 +1,16 @@
-
-
-
 def lone_sum(a, b, c):
-    if a >= b:
+    if a == b and a == c and b == c:
+        return 0
+    elif a >= b:
         return c
     elif a == c:
         return b
     elif b == c:
         return a
-    elif a == b and a == c and b == c:
-        return 0
     else:
         return a+b+c
 
-
-
-
-
-
+a1 = int(input())
+b1 = int(input())
+c1 = int(input())
+print(lone_sum(a1, b1, c1))
